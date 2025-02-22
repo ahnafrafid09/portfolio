dae2d4f829eb290f9ad87ff2e099da81af6c311c
Ahnaf <Ahnafrafid22@gmail.com>
2025-02-22 15:16:39 +0700

    edit sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,31 +3,31 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://ahnaf-rafid.verce.app",
+      url: "https://ahnaf-rafid.vercel.app",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 1,
     },
     {
-      url: "https://ahnaf-rafid.verce.app/contact",
+      url: "https://ahnaf-rafid.vercel.app/contact",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 1,
     },
     {
-      url: "https://ahnaf-rafid.verce.app/work",
+      url: "https://ahnaf-rafid.vercel.app/work",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.8,
     },
     {
-      url: "https://ahnaf-rafid.verce.app/services",
+      url: "https://ahnaf-rafid.vercel.app/services",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.5,
     },
     {
-      url: "https://ahnaf-rafid.verce.app/resume",
+      url: "https://ahnaf-rafid.vercel.app/resume",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 1,
